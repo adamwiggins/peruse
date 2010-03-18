98d4c2c2434a184fc703bb9ab3ea30768e3eea47
Adam Wiggins <adam@heroku.com>
2010-03-18 12:55:45 -0700

    feeds and posts json export

--- main.rb
+++ main.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
+require 'json'
+
 require File.dirname(__FILE__) + '/lib/all'
 
 layout 'layout'
@@ -51,6 +53,27 @@ end
 get '/posts' do
 	@posts = Post.find(:all, :conditions => "rating='thumbs_up'", :order => 'updated_at desc')
 	erb :posts
+end
+
+get '/posts.json' do
+	content_type 'application/json'
+	posts = Post.find(:all, :conditions => "rating is not null", :order => 'updated_at desc')
+	posts.map do |post|
+		{
+			:title => post.title,
+			:url => post.url,
+			:author => post.author,
+			:published_at => post.published_at,
+			:feed => post.feed.url,
+			:feed_title => post.feed.title,
+			:rating => post.rating
+		}
+	end.to_json
+end
+
+get '/feeds.json' do
+	content_type 'application/json'
+	Feed.all.map { |feed| feed.url }.to_json
 end
 
 get '/import' do
