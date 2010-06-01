b409fe77cd1c4127e3a5e783602a080c57da0e0e
Adam Wiggins <adam@heroku.com>
2010-06-01 00:32:52 -0700

    use DATABASE_URL

--- lib/all.rb
+++ lib/all.rb
@@ -1,17 +1,22 @@
-require File.dirname(__FILE__) + '/../.bundle/environment'
-Bundler.setup
+#require File.dirname(__FILE__) + '/../.bundle/environment'
+#Bundler.setup
 
 require 'active_record'
-require 'feed_tools'
-require 'nokogiri'
-require 'stalker'
+#require 'feed_tools'
+#require 'nokogiri'
+#require 'stalker'
+require 'uri'
 
-if File.exists? 'config/database.yml'
-	dbconfig = YAML.load(File.read('config/database.yml'))
-	ActiveRecord::Base.establish_connection dbconfig['production']
-else
-	ActiveRecord::Base.establish_connection :adapter => 'postgresql', :database => 'peruse'
-end
+db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/peruse')
+
+ActiveRecord::Base.establish_connection(
+	:adapter => 'postgresql',
+	:host => db.host,
+	:database => db.path.gsub(/\//, ''),
+	:username => db.user || 'postgres',
+	:password => db.password,
+	:encoding => 'utf8'
+)
 
 require File.dirname(__FILE__) + '/feed'
 require File.dirname(__FILE__) + '/post'
